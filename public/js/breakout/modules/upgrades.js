6f8d1771199ee6bde5d48612b9548ec633724b65
dustinpfister <dustin.pfister@gmail.com>
2018-09-28 22:59:39 -0400

    upgrades.js

---
+++ public/js/breakout/modules/upgrades.js
@@ -0,0 +1,24 @@
+/***********
+Upgrades.js
+
+Just a reminder of what this file is for
+
+    * EXP SYSTEM
+    * STATE OF UPGRADES
+
+***********/
+
+var Upgrades = {
+
+   exp : 0,
+   
+   points: 6,
+   
+   
+   setUpgrade : function(){
+	   
+	   
+   }
+   
+
+};
