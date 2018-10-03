8a2fe09da5d53edc6558822feb771a182997aa16
dustinpfister <dustin.pfister@gmail.com>
2018-10-03 09:01:53 -0400

    ball speed block hits upgrade

--- public/js/breakout/modules/upgrades.js
+++ public/js/breakout/modules/upgrades.js
@@ -16,22 +16,29 @@ var Upgrades = {
 
     upgrades: [
 
-        // reduce the base speed of the ball, that gets raised with each round
+        // reduce the base speed
         {
-
-            desc: 'Ball Speed Base Reduce',
+            name: 'Ball Speed Base Reduce',
+            desc: 'Reduces the base speed of the ball to begin with that is then raised via other factors',
             points: 0,
             pointsMax: 10,
-
-            // set the values in features.js
             set: function () {
+                var per = this.points / this.pointsMax;
+                Features.ballSpeedBase = Math.floor(30.2 - 25 * per);
+            }
+        },
 
+        {
+            name: 'Increase ball speed block hits',
+            desc: 'Reduces Ball speed by increasing the number of block hits needed to reach the full block hit speed multiplier',
+            points: 50,
+            pointsMax: 100,
+            set: function () {
                 var per = this.points / this.pointsMax;
 
-                Features.ballSpeedBase = (30.2 - 25 * per);
+                Features.ballBlockHitOverHits = Math.floor(10 + 990 * per);
 
             }
-
         }
 
     ],
